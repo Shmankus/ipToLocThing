2ec35fe2da8481a8dc7f92269ba5694f0d86281e
JansenL2 <jansenl2@miamioh.edu>
2026-03-02 22:53:34 -0500

    enabled focus system

--- server/index.ts
+++ server/index.ts
@@ -168,7 +168,7 @@ const startPythonProcess = () => {
     });
 };
 
-let test = true;
+let test = false; // disables the stop when unfocus
 
 // Function that stops all python processes (both since they are linked)
 // also sends a stop message to client to update UI
